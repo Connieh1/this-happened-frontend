a72827281a777a3bc9079fc4d855c8f0eacbb81f
github username <constance.hyman@mchsi.com>
2020-11-21 23:25:45 -0800

    Update URL for deployment

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-const endPoint = "http://localhost:3000/api/v1/posts"
+const endPoint = "https://this-happened-backend.herokuapp.com/api/v1/posts";
 
-document.addEventListener('DOMContentLoaded', () =>{
-  getPosts()
+document.addEventListener("DOMContentLoaded", () => {
+  getPosts();
 
   const createPostForm = document.querySelector("#create-post-form");
 
-  createPostForm.addEventListener("submit", (e) => createFormHandler(e))
-})
+  createPostForm.addEventListener("submit", (e) => createFormHandler(e));
+});
 
-document.addEventListener('DOMContentLoaded', () =>{
-  populateSelection()
-})
+document.addEventListener("DOMContentLoaded", () => {
+  populateSelection();
+});
 
-function getPosts(){
+function getPosts() {
   fetch(endPoint)
-  .then(response => response.json())
-  .then(posts => {
-    posts.data.forEach(post =>{
-      const postMarkup = `
+    .then((response) => response.json())
+    .then((posts) => {
+      posts.data.forEach((post) => {
+        const postMarkup = `
           <div data-id=${post.id}>
           <p>${post.attributes.subject.name}</p>
             <h3>${post.attributes.title}</h3>
@@ -26,53 +26,53 @@ function getPosts(){
           </div>
           <br>`;
 
-          document.querySelector('#post-container').innerHTML += postMarkup
-        })
-
-      })
+        document.querySelector("#post-container").innerHTML += postMarkup;
+      });
+    });
 }
 
 // -----
 
-function populateSelection(){
-  const url = 'http://localhost:3000/api/v1/subjects'
+function populateSelection() {
+  const url = "http://localhost:3000/api/v1/subjects";
 
-  fetch(url).then(resp => resp.json())
-  .then(data =>{
-    let subjectSelection = document.getElementById('subjects');
-    data.data.forEach(subject => {
-      let option = document.createElement("option");
-      option.setAttribute("text", subject.attributes.name)
-      option.setAttribute("value", subject.id);
-      option.innerHTML = subject.attributes.name;
-      subjectSelection.appendChild(option)
+  fetch(url)
+    .then((resp) => resp.json())
+    .then((data) => {
+      let subjectSelection = document.getElementById("subjects");
+      data.data.forEach((subject) => {
+        let option = document.createElement("option");
+        option.setAttribute("text", subject.attributes.name);
+        option.setAttribute("value", subject.id);
+        option.innerHTML = subject.attributes.name;
+        subjectSelection.appendChild(option);
+      });
     });
-  });
 }
 
-function createFormHandler(e){
-  e.preventDefault()
-  const titleInput = document.querySelector('#input-title').value;
-  const descriptionInput = document.querySelector('#input-description').value;
-  const subjectId= parseInt(document.querySelector('#subjects').value);
-  postFetch(titleInput, descriptionInput, subjectId)
+function createFormHandler(e) {
+  e.preventDefault();
+  const titleInput = document.querySelector("#input-title").value;
+  const descriptionInput = document.querySelector("#input-description").value;
+  const subjectId = parseInt(document.querySelector("#subjects").value);
+  postFetch(titleInput, descriptionInput, subjectId);
 }
 
-function postFetch(title, description, subject_id){
+function postFetch(title, description, subject_id) {
   fetch(endPoint, {
     //Post request
     method: "POST",
-    headers: {"Content-Type": "application/json"},
+    headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
       title: title,
       description: description,
-      subject_id: subject_id
-    })
+      subject_id: subject_id,
+    }),
   })
-  .then(response => response.json())
-  .then(post => {
-    const postData = post.data.attributes
-    const postMarkup =`
+    .then((response) => response.json())
+    .then((post) => {
+      const postData = post.data.attributes;
+      const postMarkup = `
     <div data-id=${post.id}>
     <h3>${postData.title}</h3>
     <p>${postData.subject.name}</p>
@@ -80,6 +80,6 @@ function postFetch(title, description, subject_id){
     </div>
     <br>`;
 
-    document.querySelector('#post-container').innerHTML += postMarkup;
-  })
+      document.querySelector("#post-container").innerHTML += postMarkup;
+    });
 }
